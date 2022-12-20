54bf43ed06e6b3afc23fe1d71edcc48e099d238a
John Risk <john.risk@adigium.com>
2022-12-20 12:06:27 +0000

    restart-server: writing node info to public file

---
+++ scripts/restart-server.sh
@@ -0,0 +1,29 @@
+cd /home/adigium/eth-pos-devnet/
+
+docker compose down
+
+# Clean Folders
+#git clean -fxd
+rm -rf consensus/beacondata/ consensus/genesis.ssz consensus/validatordata/ execution/geth/
+
+# Initialize Genesis
+#docker compose -f docker-initialize.yml up && docker compose -f docker-initialize.yml down
+docker compose -f docker-initialize.yml run --rm geth-genesis
+docker compose -f docker-initialize.yml run --rm create-beacon-chain-genesis
+
+# Run Nodes
+#docker compose -f docker-run.yml up -d
+docker compose -f docker-run.yml up geth -d
+sleep 5
+docker compose -f docker-run.yml up beacon-chain -d
+sleep 5
+docker compose -f docker-run.yml up validator -d
+
+# Write node info
+scripts/collectNodeInfo.sh > /var/www/html/adigium/nodeinfo.txt
+
+# Show Log Commands
+echo You can watch the log file
+echo "	docker logs eth-pos-devnet-geth-1 -f"
+echo "	docker logs eth-pos-devnet-beacon-chain-1 -f"
+echo "	docker logs eth-pos-devnet-validator-1 -f"
