2594b9a088cceca5d272c383ce8e54504ebeb640
John Risk <john.risk@adigium.com>
2022-12-20 14:38:32 +0000

    collectNodeInfo: removed export keyword

--- scripts/collectNodeInfo.sh
+++ scripts/collectNodeInfo.sh
@@ -1,18 +1,19 @@
-export server_ip=`curl ifconfig.me 2>/dev/null` \
+server_ip=`curl ifconfig.me 2>/dev/null` \
         && echo export server_ip=$server_ip
 echo
 
-export bootgeth=`docker logs eth-pos-devnet-geth-1 2>&1 | grep self=enode | sed s/.*self=//g | sed s/@127.0.0.1:30303/@${server_ip}:30303/g | tail -1` \
+bootgeth=`docker logs eth-pos-devnet-geth-1 2>&1 | grep self=enode | sed s/.*self=//g | sed s/@127.0.0.1:30303/@${server_ip}:30303/g | tail -1` \
         && echo export bootgeth=$bootgeth
 echo
 
-export bootbeacon=`curl http://localhost:8080/p2p 2>/dev/null | grep self= | sed s/',\\'/'ip4.*'//g | sed s/'self='//g` \
+bootbeacon=`curl http://localhost:8080/p2p 2>/dev/null | grep self= | sed s/',\\'/'ip4.*'//g | sed s/'self='//g` \
         && echo export bootbeacon=$bootbeacon
 echo
 
-export bootipfs=/ip4/${server_ip}/tcp/4001/ipfs/`ipfs id|grep \"ID\"|sed s/'.*\"ID\": \"'//|sed s/'\",$'//` \
+bootipfs=/ip4/${server_ip}/tcp/4001/ipfs/`ipfs id|grep \"ID\"|sed s/'.*\"ID\": \"'//|sed s/'\",$'//` \
 	&& echo export bootipfs=$bootipfs
 echo
 
-export bootipfscluster=/ip4/${server_ip}/tcp/9096/ipfs/`ipfs-cluster-ctl id | grep IPFS | sed s/'.*IPFS: '//g` \
+bootipfscluster=/ip4/${server_ip}/tcp/9096/ipfs/`ipfs-cluster-ctl id | grep IPFS | sed s/'.*IPFS: '//g` \
 	&& echo export bootipfscluster=$bootipfscluster
+echo
