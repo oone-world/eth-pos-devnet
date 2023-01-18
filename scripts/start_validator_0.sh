8ca58a357527205ebe27a30ea9bf6c1c5c6bc3f4
john-risk-adigium <john.risk@adigium.com>
2023-01-18 16:22:48 +0000

    Added services

---
+++ scripts/start_validator_0.sh
@@ -0,0 +1,2 @@
+cd /home/adigium/eth-pos-devnet
+lighthouse vc --testnet-dir /home/adigium/eth-pos-devnet/data/testnet --datadir /home/adigium/eth-pos-devnet/data/validator/0 --beacon-nodes http://localhost:5052 --graffiti ProducedBy_Validator_2 --suggested-fee-recipient 0x90e0d0b7d7CdBb79934f5B1F81efAC5689142775
---
+++ scripts/start_validator_1.sh
@@ -0,0 +1,2 @@
+cd /home/adigium/eth-pos-devnet
+lighthouse vc --testnet-dir /home/adigium/eth-pos-devnet/data/testnet --datadir /home/adigium/eth-pos-devnet/data/validator/1 --beacon-nodes http://localhost:5053 --graffiti ProducedBy_Validator_3 --suggested-fee-recipient 0xe77ce92F67d19fCc00f68550746260d4DCbcAd0C
